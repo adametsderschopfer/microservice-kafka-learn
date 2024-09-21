8ba0ae45d75ea77e21453f5739238a16070e3432
Vladislav Adamets <adametsderschopfer@yandex.ru>
2024-09-21 23:06:38 +0200

    feat: add user register topic sent message to kafka

--- microservices/user/user.service.js
+++ microservices/user/user.service.js
@@ -1,13 +1,42 @@
 import Fastify from 'fastify'
 import kafka from "@fastify/kafka"
-
-import {handleUserRegister} from "./handlers/register.js";
+import {KAFKA_USER_REGISTER_TOPIC} from "./constants.js";
 
 const fastify = Fastify({
     logger: true,
 })
 
-fastify.post('/user/register', handleUserRegister)
+fastify.register(kafka, {
+    producer: {
+        'metadata.broker.list': 'kafka:9092',
+        'fetch.wait.max.ms': 10,
+        'fetch.error.backoff.ms': 50,
+        'client.id': 'user-service',
+        'dr_cb': true
+    },
+});
+
+fastify.post('/user/register', async (request, reply) => {
+    // .....user register logic.....
+    // The creation logic was skipped for the sake of studying message exchange between services
+    // ...
+
+    /*
+    * Send event to create profile for successfully registered user
+    * */
+    fastify.kafka.push({
+        topic: KAFKA_USER_REGISTER_TOPIC,
+        payload: JSON.stringify({
+            id: Date.now(),
+            email: "user@example.com",
+            username: "imbatman"
+        }),
+    })
+
+    reply.send({
+        message: "User successfully created!"
+    })
+})
 
 fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
     if (err) throw err
